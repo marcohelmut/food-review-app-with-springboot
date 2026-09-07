36029858200d66decd05a311d385ebe3bb720a61
marcohelmut <kmhbmorales@gmail.com>
2026-09-07 12:00:45 +0800

    switch to controller level field validation with springboot starter validation

---
+++ src/main/java/com/marcohelmut/foodreviewapp/controllers/AppController.java
@@ -0,0 +1,37 @@
+package com.marcohelmut.foodreviewapp.controllers;
+
+import com.marcohelmut.foodreviewapp.entities.Food;
+import com.marcohelmut.foodreviewapp.entities.Stall;
+import com.marcohelmut.foodreviewapp.services.AppService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class AppController {
+
+    private final AppService appService;
+
+    @Autowired
+    public AppController(AppService appService) {
+        this.appService = appService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Stall> createStall(@Valid @RequestBody Stall stall) {
+        Stall savedStall = appService.saveStall(stall);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedStall);
+    }
+
+    @PostMapping
+    public ResponseEntity<Food> createFood(@Valid @RequestBody Food food) {
+        Food savedFood = appService.saveFood(food);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedFood);
+    }
+}
