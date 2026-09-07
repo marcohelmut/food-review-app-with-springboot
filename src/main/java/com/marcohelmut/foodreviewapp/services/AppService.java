85f88efa74d92daeec2d6fdf225182611056d3cf
marcohelmut <kmhbmorales@gmail.com>
2026-09-07 15:02:34 +0800

    seperated service classes to adhere to SOLID principles

---
+++ src/main/java/com/marcohelmut/foodreviewapp/services/AppService.java
@@ -0,0 +1,39 @@
+package com.marcohelmut.foodreviewapp.services;
+
+import com.marcohelmut.foodreviewapp.entities.Food;
+import com.marcohelmut.foodreviewapp.entities.Review;
+import com.marcohelmut.foodreviewapp.entities.Stall;
+import com.marcohelmut.foodreviewapp.repositories.FoodRepository;
+import com.marcohelmut.foodreviewapp.repositories.ReviewRepository;
+import com.marcohelmut.foodreviewapp.repositories.StallRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AppService {
+
+    private final StallRepository stallRepository;
+    private final FoodRepository foodRepository;
+    private final ReviewRepository reviewRepository;
+
+    @Autowired
+    public AppService(StallRepository stallRepository, FoodRepository foodRepository, ReviewRepository reviewRepository) {
+        this.stallRepository = stallRepository;
+        this.foodRepository = foodRepository;
+        this.reviewRepository = reviewRepository;
+    }
+
+    public Stall saveStall(Stall stall) {
+        stall.setName(stall.getName().trim());
+        return stallRepository.save(stall);
+    }
+
+    public Food saveFood(Food food) {
+        food.setName(food.getName().trim());
+        return foodRepository.save(food);
+    }
+
+    public Review saveReview(Review review) {
+        return reviewRepository.save(review);
+    }
+}
